ba950dfb4826467722a3795602fcfa0615aa4cfe
jackb333 <42563271+jackb333@users.noreply.github.com>
2018-08-23 12:57:17 -0500

    Hello source code

---
+++ Hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jack Barrett
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jack Barrett Mech E\n");
+
+  return 0;
+}
